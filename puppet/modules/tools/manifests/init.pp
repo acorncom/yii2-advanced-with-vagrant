cb80aa867ee972466333cde56f0182a36324e31b
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2013-07-24 15:35:48 +0200

    Magerun

--- puppet/modules/tools/manifests/init.pp
+++ puppet/modules/tools/manifests/init.pp
@@ -3,5 +3,6 @@ class tools {
     include tools::installer
     include tools::modman
     include tools::composer
+    include tools::magerun
 
 }
---
+++ puppet/modules/tools/manifests/magerun.pp
@@ -0,0 +1,16 @@
+class tools::magerun {
+
+    exec { "install magerun":
+        cwd     => "/usr/local/bin",
+        creates => "/usr/local/bin/n98-magerun.phar",
+        command => "wget --no-check-certificate https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar",
+        require => [ Package['wget'], Package['php5'] ],
+    }
+
+    -> exec { "chmod magerun":
+        cwd     => "/usr/local/bin",
+        onlyif  => "test `stat --format '%a' n98-magerun.phar` -neq 755",
+        command => "chmod 755 n98-magerun.phar",
+    }
+
+}
