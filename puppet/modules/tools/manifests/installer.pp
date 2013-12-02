477e3a1d76a250181c160e8e8fddced5679da880
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2013-12-02 17:54:02 +0100

    Updated installer install script

--- puppet/modules/tools/manifests/installer.pp
+++ puppet/modules/tools/manifests/installer.pp
@@ -1,26 +1,26 @@
 class tools::installer {
 
-    $repo_path = '/usr/local/src/Installer'
-    $binary = '/usr/local/bin/Installer'
-    $url = 'https://github.com/jacquesbh/Installer.git'
+    $repo_path = '/usr/local/src/installer'
+    $binary = '/usr/local/bin/installer'
+    $url = 'git://github.com/jacquesbh/installer.git'
 
-    exec { "clone jacquesbh/Installer":
+    exec { "clone jacquesbh/installer":
         creates => $repo_path,
         command => "git clone $url $repo_path",
         require => Package['git'],
     }
 
-    -> file { "link Installer bin":
+    -> file { "link installer bin":
         ensure  => link,
         path    => $binary,
-        target  => "$repo_path/bin/Installer",
+        target  => "$repo_path/bin/installer",
     }
 
-    exec { "pull jacquesbh/Installer":
+    exec { "pull jacquesbh/installer":
         cwd     => $repo_path,
         onlyif  => "test `git log -1 | grep commit | awk '{ print \$2 }'` != `git ls-remote $url | grep master | awk '{ print \$1 }'`",
         command => 'git pull',
-        require => Exec['clone jacquesbh/Installer'],
+        require => Exec['clone jacquesbh/installer'],
     }
 
 }
