2381fe4072726593a3184639f1483c06558a5fa2
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2013-12-05 23:38:43 +0100

    Added packages screen, vim and htop

--- puppet/modules/server/manifests/init.pp
+++ puppet/modules/server/manifests/init.pp
@@ -17,7 +17,7 @@ class server ( $hostname ) {
     }
 
     # Few packages
-    $packages = ["curl", "tidy"]
+    $packages = ["curl", "tidy", "screen", "vim", "htop"]
     package { $packages:
         ensure  => latest,
         require => Exec['update'],
