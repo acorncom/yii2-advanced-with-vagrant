11c58155de0bb922f3649fc908a403f74c84d01f
David Baker <dbaker@acorncomputersolutions.com>
2014-02-27 11:00:33 +0300

    Setup the basic Yii2 app advanced inside of Vagrant

---
+++ www/backend/controllers/SiteController.php
@@ -0,0 +1,75 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\web\AccessControl;
+use yii\web\Controller;
+use common\models\LoginForm;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors()
+	{
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+				'rules' => [
+					[
+						'actions' => ['login', 'error'],
+						'allow' => true,
+					],
+					[
+						'actions' => ['logout', 'index'],
+						'allow' => true,
+						'roles' => ['@'],
+					],
+				],
+			],
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function actions()
+	{
+		return [
+			'error' => [
+				'class' => 'yii\web\ErrorAction',
+			],
+		];
+	}
+
+	public function actionIndex()
+	{
+		return $this->render('index');
+	}
+
+	public function actionLogin()
+	{
+		if (!\Yii::$app->user->isGuest) {
+			return $this->goHome();
+		}
+
+		$model = new LoginForm();
+		if ($model->load(Yii::$app->request->post()) && $model->login()) {
+			return $this->goBack();
+		} else {
+			return $this->render('login', [
+				'model' => $model,
+			]);
+		}
+	}
+
+	public function actionLogout()
+	{
+		Yii::$app->user->logout();
+		return $this->goHome();
+	}
+}
