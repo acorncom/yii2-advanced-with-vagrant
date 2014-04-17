07a97cc8697ff45dfc8181262536a27f0c3e1cc5
David Baker <dbaker@acorncomputersolutions.com>
2014-04-17 16:45:42 -0600

    Fixed console config errors if no overrides are setup

--- www/console/config/main.php
+++ www/console/config/main.php
@@ -1,9 +1,15 @@
 <?php
+$commonParamsLocalPath = __DIR__ . '/../../common/config/params-local.php';
+$commonParamsLocal = (file_exists($commonParamsLocalPath)) ? require($commonParamsLocalPath) : array();
+
+$paramsLocalPath = __DIR__ . '/params-local.php';
+$paramsLocal = (file_exists($paramsLocalPath)) ? require($paramsLocalPath) : array();
+
 $params = array_merge(
-	require(__DIR__ . '/../../common/config/params.php'),
-	require(__DIR__ . '/../../common/config/params-local.php'),
-	require(__DIR__ . '/params.php'),
-	require(__DIR__ . '/params-local.php')
+    require(__DIR__ . '/../../common/config/params.php'),
+    $commonParamsLocal,
+    require(__DIR__ . '/params.php'),
+    $paramsLocal
 );
 
 return [
